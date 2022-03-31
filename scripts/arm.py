ea4ed76d77aa9f367015a27c9e24d06229602f85
David Meira <74539212+dmeirap@users.noreply.github.com>
2022-03-31 09:03:34 +0000

    Add files via upload

---
+++ scripts/arm.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import asyncio
+from mavsdk import System
+
+
+async def run():
+    drone = System(mavsdk_server_address="localhost", port=50051)
+    await drone.connect(system_address="serial:///dev/ttyUSB0:921600")
+
+    print("Esperando la conexion del dron...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("Dron conectado!")
+            break
+
+    print("Esperando la posicion global del dron...")
+    async for health in drone.telemetry.health():
+        if health.is_global_position_ok:
+            print("Posicion global estimada OK")
+            break
+
+    #print("Fetching amsl altitude at home location....")
+    async for terrain_info in drone.telemetry.home():
+        absolute_altitude = terrain_info.absolute_altitude_m
+        break
+
+    print("-- Arming")
+    await drone.action.arm()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
